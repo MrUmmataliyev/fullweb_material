3c9876e3248f7874bd72007ee196e6e9d270a0e5
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-04-16 17:06:58 +0500

    just

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GetallComponent } from './components/getall/getall.component';
+import { CreateComponent } from './components/create/create.component';
 
 const routes: Routes = [
   {path:"", component:GetallComponent},
-  {path:"Getall", component:GetallComponent},
+  {path:"getall", component:GetallComponent},
+  {path:"create", component:CreateComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,21 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { GetallComponent } from './components/getall/getall.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import { CreateComponent } from './components/create/create.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   declarations: [
     AppComponent,
     GetallComponent,
+    CreateComponent,
+    NavbarComponent,
 
   ],
   imports: [
@@ -21,7 +30,14 @@ import {MatTableModule} from '@angular/material/table';
     MatSlideToggleModule,
     HttpClientModule,
     RouterModule,
-    MatTableModule
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatDividerModule,
+    MatButtonModule,
+    FormsModule,
+
 
     
   ],
